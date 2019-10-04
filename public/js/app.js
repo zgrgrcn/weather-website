dac8a836d0e528eedbddf37ede315b896bd3148d
zgrgrcn <ozgurgurcan1996@gmail.com>
2019-10-04 13:33:45 +0300

    Inıt cocmmit

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+console.log('Client side javascript file is loaded!')
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                message1.textContent = data.error
+                message2.textContent = ''
+            } else {
+                message1.textContent = data.location
+                message2.textContent = data.forecast
+            }
+        })
+    })
+})
